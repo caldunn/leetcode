38b8145cc0cfe1b1aa0a1f05afb984af47a36680
Caleb Dunn <caldunn@outlook.com>
2022-06-23 11:46:47 +0800

    Manual toSet functionality to allow for short circuit.

--- src/main/scala/ArraysAndHashing/ContainsDuplicates.scala
+++ src/main/scala/ArraysAndHashing/ContainsDuplicates.scala
@@ -1,8 +1,15 @@
 package ArraysAndHashing
 import scala.collection.mutable
 
-// Very easy lmao
+// Very easy lmao -> Not very functional.
+/* */
 object ContainsDuplicates {
   def containsADuplicate(input: Seq[Int]): Boolean =
-    input.toSet[Int].size != input.size
+    val mUniqueSet = mutable.Set[Int]()
+    for i <- input do
+      val isAdded = mUniqueSet.add(i)
+      if !isAdded then return true
+
+    false
+
 }
